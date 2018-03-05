5976a050dadd0ce4c6bce1e6a129d0440371a444
Aries Cs <aries0d0f@gmail.com>
2018-03-05 20:58:44 +0800

    Modify menu text.

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,8 +2,8 @@
   <div class="menu">
     <router-link to="/" class="logo"><img src="~assets/images/logo.png" alt=""><span>{{ $nuxt.$options.head.title }}</span></router-link>
     <ul class="nav">
-      <li><router-link to="/post">文章列表</router-link></li>
-      <li><router-link to="/category">文章分類</router-link></li>
+      <li><router-link to="/post">Archive</router-link></li>
+      <li><router-link to="/category">Category</router-link></li>
     </ul>
     <ul class="toolbox">
       <li><font-awesome-icon :icon="['fas', 'search']" /></li>
