094d99470acebafd305e8f3d72d5ea25e9509ea0
Aries Cs <aries0d0f@gmail.com>
2018-03-05 15:00:03 +0800

    Add menu components.

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="menu">
+    <div class="logo">
+      <router-link to="/"><span>{{ $nuxt.$options.head.title }}</span></router-link>
+    </div>
+    <ul class="nav">
+      <li><router-link to="/post">文章列表</router-link></li>
+      <li><router-link to="/category">文章分類</router-link></li>
+    </ul>
+    <ul class="toolbox">
+      <li><font-awesome-icon :icon="['fas', 'search']" /></li>
+    </ul>
   </div>
 </template>
 
