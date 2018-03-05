0860c654a62209af24478f21f6eba964a168be71
Aries Cs <aries0d0f@gmail.com>
2018-03-05 22:48:46 +0800

    Add sample banner.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <div class="banner" :style="{ 'background-image': `url(${banner.image})` }">
+      <div class="content">
+        <h1 class="title">{{ $nuxt.$options.head.title }}</h1>
+        <p class="subtitle">{{ banner.subtitle }}</p>
+      </div>
+    </div>
     <section class="container">
     </section>
   </div>
@@ -7,6 +13,13 @@
 
 <script>
 export default {
-  
+  data () {
+    return {
+      banner: {
+        image: 'https://images.unsplash.com/photo-1517583106829-61e14fcbddf5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a8a51e7f1bf1b8bc291eacdb9b912af2&fit=crop&w=1000&h=1000&q=80',
+        subtitle: 'A lightweight blog integration system based on Node.js'
+      }
+    }
+  }
 }
 </script>
