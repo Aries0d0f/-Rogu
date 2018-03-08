d41493811c72ab3adba7d0098e4d171a516b1d41
Aries Cs <aries0d0f@gmail.com>
2018-03-08 12:39:03 +0800

    Add error event handler.

--- store/post.js
+++ store/post.js
@@ -12,18 +12,26 @@ const getters = {
 
 const actions = {
   async getAllPost ({ commit }) {
-    if (state().post.length > 0) {
-      return state.post
+    try {
+      if (state().post.length > 0) {
+        return state.post
+      }
+      let res = await axios.get(`/api/post`)
+      res.data.data.sort((a, b) => { return new Date(a.date).getTime() - new Date(b.date).getTime() }).reverse()
+      commit('GET_ALL_POST', res.data.data)
+      return res.data.data
+    } catch (err) {
+      throw new Error(err)
     }
-    let res = await axios.get(`/api/post`)
-    res.data.data.sort((a, b) => { return new Date(a.date).getTime() - new Date(b.date).getTime() }).reverse()
-    commit('GET_ALL_POST', res.data.data)
-    return res.data.data
   },
 
   async getPostByID ({ commit }, pid) {
-    let res = await axios.get(`/api/post/${pid}`)
-    return res.data.data
+    try {
+      let res = await axios.get(`/api/post/${pid}`)
+      return res.data.data
+    } catch (err) {
+      throw new Error(err)
+    }
   }
 }
 
